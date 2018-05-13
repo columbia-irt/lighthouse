14770bdb89ba3242e487fef29ba255f157214a4d
Jan Janak <jan@janakj.org>
2018-05-13 18:55:54 -0400

    Don't perform implicit PWM-GPIO conversion.
    Before, the Pin object would automatically use setState method
    if the value provided by upper layers is min or maximum. We no longer
    do that, because we don't know the range of PWM values in advance.
    It is better to let upper layers to perform the conversion explicitly
    if necessary.

--- pigpio/src/main/java/io/sece/pigpio/PiGPIOPin.java
+++ pigpio/src/main/java/io/sece/pigpio/PiGPIOPin.java
@@ -3,16 +3,17 @@ package io.sece.pigpio;
 import io.sece.pigpio.PiGPIO;
 import io.sece.pigpio.PiGPIOException;
 
+
 public class PiGPIOPin
 {
-    private int gpio; //Number of the Hardware GPIO pin
+    private int gpio;
 
 
     public PiGPIOPin(int gpio) throws PiGPIOException
     {
         this.gpio = gpio;
 
-        PiGPIO.gpioSetMode(gpio,PiGPIO.PI_OUTPUT); // GIPO pin is set to Output since this is what we need
+        PiGPIO.gpioSetMode(gpio,PiGPIO.PI_OUTPUT);
     }
 
 
@@ -22,34 +23,18 @@ public class PiGPIOPin
     }
 
 
-    public void setState(boolean on) throws PiGPIOException
+    public void setState(boolean state) throws PiGPIOException
     {
-        if (on) {
-            PiGPIO.gpioWrite(this.gpio, 1);
-        } else {
-            PiGPIO.gpioWrite(this.gpio, 0);
-        }
+        if (state) PiGPIO.gpioWrite(this.gpio, 1);
+        else PiGPIO.gpioWrite(this.gpio, 0);
     }
 
 
-    public void setValue(int value) throws PiGPIOException
+    public void setPWMValue(int value) throws PiGPIOException
     {
-        if (value >= 0 && value <=255) {
-            switch(value) {
-            case 0:
-                this.setState(false);
-                break;
-
-            case 255:
-                this.setState(true);
-                break;
-
-            default:
-                PiGPIO.gpioPWM(this.gpio, value);
-                break;
-            }
-        } else {
-            throw new IllegalArgumentException();
+        if (value < 0 || value > 255) {
+            throw new IllegalArgumentException("Invalid value: " + value);
         }
+        PiGPIO.gpioPWM(gpio, value);
     }
 }
