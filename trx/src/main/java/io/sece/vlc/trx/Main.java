078a9579bc4ab68c19ca673f4e292dc9db0b63a5
ninja2009 <hagen-ninja@hotmail.de>
2018-05-16 17:50:43 +0200

    Calibrationmode Added without restarting the tool

--- trx/src/main/java/io/sece/vlc/trx/Main.java
+++ trx/src/main/java/io/sece/vlc/trx/Main.java
@@ -1,6 +1,7 @@
 package io.sece.vlc.trx;
 
 import java.lang.IllegalArgumentException;
+import java.util.Scanner;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ExecutorService;
 
@@ -16,7 +17,11 @@ import io.sece.vlc.FSK8Modulator;
 
 public class Main {
 
+
     public static void main(String[] args) throws Exception {
+
+        boolean calibrate = true;
+
         System.out.println("Starting LED transmitter");
         ExecutorService threadPool = Executors.newCachedThreadPool();
 
@@ -54,20 +59,49 @@ public class Main {
         FSK4Modulator mod2 = new FSK4Modulator();
         FSK8Modulator mod3 = new FSK8Modulator();
         OOKModulator  mod4 = new OOKModulator();
-        CalibrationModulator mod5 = new CalibrationModulator(180,100,100);
 
-        // Create an transmitter implementation which connects a particular
-        // LEDInterface object to a particular Modulator. Note this should
-        // enforce strict type checking and it should not be possible to
-        // connect LEDs with incompatible modulators. That should generate a compile-time error.
-        Transmitter<?> t = new Transmitter<>(led3, mod5, 2000);
 
-        // Transmit the data stored in the buffer.
-        t.tx(data);
+        Scanner in = new Scanner(System.in);
 
-        //testing purpose, make sure that the LED is off after any transmission
-        led3.set(Color.BLACK);
+        if(calibrate) {
+            while (true) {
+                System.out.println("Hue Wert eingeben");
+                String hueFromLine = in.nextLine();
+                int hue = Integer.parseInt(hueFromLine);
+                System.out.println("Hue Wert: " + hue);
 
-        System.out.println("Done!");
+                CalibrationModulator mod5 = new CalibrationModulator(hue, 100, 100);
+
+                // Create an transmitter implementation which connects a particular
+                // LEDInterface object to a particular Modulator. Note this should
+                // enforce strict type checking and it should not be possible to
+                // connect LEDs with incompatible modulators. That should generate a compile-time error.
+                Transmitter<?> t = new Transmitter<>(led3, mod5, 2000);
+
+                // Transmit the data stored in the buffer.
+                t.tx(data);
+
+                //testing purpose, make sure that the LED is off after any transmission
+                led3.set(Color.BLACK);
+
+                System.out.println("Done!");
+            }
+        }
+        else
+        {
+            // Create an transmitter implementation which connects a particular
+            // LEDInterface object to a particular Modulator. Note this should
+            // enforce strict type checking and it should not be possible to
+            // connect LEDs with incompatible modulators. That should generate a compile-time error.
+            Transmitter<?> t = new Transmitter<>(led3, mod3, 2000);
+
+            // Transmit the data stored in the buffer.
+            t.tx(data);
+
+            //testing purpose, make sure that the LED is off after any transmission
+            led3.set(Color.BLACK);
+
+            System.out.println("Done!");
+        }
     }
 }
