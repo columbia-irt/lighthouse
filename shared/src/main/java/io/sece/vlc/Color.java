bb2c821db76028ce097f11b2f5c2378dc069042c
ninja2009 <hagen-ninja@hotmail.de>
2018-06-05 17:05:33 +0200

    Color Merge

--- shared/src/main/java/io/sece/vlc/Color.java
+++ shared/src/main/java/io/sece/vlc/Color.java
@@ -8,6 +8,14 @@ package io.sece.vlc;
 public class Color {
     public int red, green, blue;
 
+    public static final Color RED = new Color(255, 0, 0);
+    public static final Color GREEN = new Color(0, 255, 0);
+    public static final Color BLUE = new Color(0, 0, 255);
+    public static final Color PURPLE = new Color(255, 0, 190);
+    public static final Color YELLOW = new Color(255, 185, 0);
+    public static final Color TURQUIOSE = new Color(0, 255, 150);
+    public static final Color BLACK = new Color(0, 0, 0);
+    public static final Color WHITE = new Color(255, 255, 255);
 
     public Color(int red, int green, int blue) {
         this.red = red;
@@ -15,6 +23,17 @@ public class Color {
         this.blue = blue;
     }
 
+    public int getRed() {
+        return red;
+    }
+
+    public int getGreen() {
+        return green;
+    }
+
+    public int getBlue() {
+        return blue;
+    }
 
 
     public static int HSBtoRGB(float var0, float var1, float var2) {
@@ -22,31 +41,57 @@ public class Color {
         int var4 = 0;
         int var5 = 0;
         if (var1 == 0.0F) {
+            var3 = var4 = var5 = (int) (var2 * 255.0F + 0.5F);
         } else {
+            float var6 = (var0 - (float) Math.floor((double) var0)) * 6.0F;
+            float var7 = var6 - (float) Math.floor((double) var6);
             float var8 = var2 * (1.0F - var1);
             float var9 = var2 * (1.0F - var1 * var7);
             float var10 = var2 * (1.0F - var1 * (1.0F - var7));
+            switch ((int) var6) {
                 case 0:
+                    var3 = (int) (var2 * 255.0F + 0.5F);
+                    var4 = (int) (var10 * 255.0F + 0.5F);
+                    var5 = (int) (var8 * 255.0F + 0.5F);
                     break;
                 case 1:
+                    var3 = (int) (var9 * 255.0F + 0.5F);
+                    var4 = (int) (var2 * 255.0F + 0.5F);
+                    var5 = (int) (var8 * 255.0F + 0.5F);
                     break;
                 case 2:
+                    var3 = (int) (var8 * 255.0F + 0.5F);
+                    var4 = (int) (var2 * 255.0F + 0.5F);
+                    var5 = (int) (var10 * 255.0F + 0.5F);
                     break;
                 case 3:
+                    var3 = (int) (var8 * 255.0F + 0.5F);
+                    var4 = (int) (var9 * 255.0F + 0.5F);
+                    var5 = (int) (var2 * 255.0F + 0.5F);
                     break;
                 case 4:
+                    var3 = (int) (var10 * 255.0F + 0.5F);
+                    var4 = (int) (var8 * 255.0F + 0.5F);
+                    var5 = (int) (var2 * 255.0F + 0.5F);
                     break;
                 case 5:
+                    var3 = (int) (var2 * 255.0F + 0.5F);
+                    var4 = (int) (var8 * 255.0F + 0.5F);
+                    var5 = (int) (var9 * 255.0F + 0.5F);
             }
         }
 
         return -16777216 | var3 << 16 | var4 << 8 | var5 << 0;
     }
 
+    public static Color hsvToRGB(float hue, float saturation, float brightness) {
         float hueT = hue / 360;
         float saturationT = saturation / 100;
         float brightnessT = brightness / 100;
         int rgb = HSBtoRGB(hueT, saturationT, brightnessT);
+        int red = (rgb >> 16) & 0xFF;
+        int green = (rgb >> 8) & 0xFF;
+        int blue = rgb & 0xFF;
 
         return new Color(red, green, blue);
     }
@@ -66,8 +111,10 @@ public class Color {
             var8 = var2;
         }
 
+        float var6 = (float) var7 / 255.0F;
         float var5;
         if (var7 != 0) {
+            var5 = (float) (var7 - var8) / (float) var7;
         } else {
             var5 = 0.0F;
         }
@@ -76,6 +123,9 @@ public class Color {
         if (var5 == 0.0F) {
             var4 = 0.0F;
         } else {
+            float var9 = (float) (var7 - var0) / (float) (var7 - var8);
+            float var10 = (float) (var7 - var1) / (float) (var7 - var8);
+            float var11 = (float) (var7 - var2) / (float) (var7 - var8);
             if (var0 == var7) {
                 var4 = var11 - var10;
             } else if (var1 == var7) {
@@ -90,6 +140,9 @@ public class Color {
             }
         }
 
+        var3[0] = ((int) (var4 * 360) * 100) / 100;
+        var3[1] = ((int) (var5 * 100) * 100) / 100;
+        var3[2] = ((int) (var6 * 100) * 100) / 100;
         return var3;
     }
 }
