617db40363b6ea45eae8135c3b11e5c757b98c6e
Jan Janak <jan@janakj.org>
2018-07-05 11:11:00 -0400

    Add a LED driver for the text console.
    This driver emulates an RGB led by changing the color of a rectangle
    on the ANSI text console. The driver can only display the basic set of
    colors: black, white, red, green, blue, yellow, cyan, and magenta. If
    a different color is passed to it, it will display the nearest
    matching color from that palette.

---
+++ trx/src/main/java/io/sece/vlc/trx/ConsoleLED.java
@@ -0,0 +1,81 @@
+package io.sece.vlc.trx;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import io.sece.vlc.Color;
+
+
+class ConsoleLED implements ColorLEDInterface {
+    private static final String PREFIX  = "\033[";
+    private static final String HOME    = "0;0H";
+    private static final String CLEAR   = "2J";
+    private static final String RESET   = "0m";
+    private static final String BLACK   = "40m";
+    private static final String WHITE   = "47m";
+    private static final String RED     = "41m";
+    private static final String GREEN   = "42m";
+    private static final String BLUE    = "44m";
+    private static final String YELLOW  = "43m";
+    private static final String CYAN    = "46m";
+    private static final String MAGENTA = "45m";
+
+    private static final Map<Color, String> colorMap;
+
+    static {
+        colorMap = new HashMap<>();
+        colorMap.put(Color.BLACK,   BLACK);
+        colorMap.put(Color.WHITE,   WHITE);
+        colorMap.put(Color.RED,     RED);
+        colorMap.put(Color.GREEN,   GREEN);
+        colorMap.put(Color.BLUE,    BLUE);
+        colorMap.put(Color.YELLOW,  YELLOW);
+        colorMap.put(Color.CYAN,    CYAN);
+        colorMap.put(Color.MAGENTA, MAGENTA);
+    }
+
+    private static void home() {
+        System.out.print(PREFIX + HOME);
+    }
+
+
+    private static void clear() {
+        System.out.print(PREFIX + CLEAR);
+    }
+
+
+    private static void setColor(String color) {
+        System.out.print(PREFIX + color);
+    }
+
+
+    private static void resetColor() {
+        System.out.print(PREFIX + RESET);
+    }
+
+
+    private static void draw(String color) {
+        home();
+        for(int i = 0; i < 5; i++) {
+            setColor(color);
+            System.out.print("          ");
+            resetColor();
+            System.out.println();
+        }
+    }
+
+
+    private String colorToEscape(Color color) {
+        color = color.nearestNeighbor(Color.BLACK, Color.WHITE, Color.RED, Color.GREEN, Color.BLUE, Color.YELLOW, Color.CYAN, Color.MAGENTA);
+        String rv = colorMap.get(color);
+        if (rv == null)
+            throw new RuntimeException("Bug in ConsoleLED implementation");
+        return rv;
+    }
+
+
+    @Override
+    public void set(Color color) {
+        draw(colorToEscape(color));
+    }
+}
