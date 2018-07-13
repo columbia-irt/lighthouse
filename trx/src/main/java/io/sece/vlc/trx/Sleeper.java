14cf9661cdcd037244a78044830fd572ce778b53
Jan Janak <jan@janakj.org>
2018-07-13 09:34:26 -0400

    Improvements to transmitter timing.
    - Use nanoseconds to keep track of all timing intervals
    - When calculating remaining time to wait, take into account the time
      it takes to execute the modulator and LED driver functions.

--- trx/src/main/java/io/sece/vlc/trx/Sleeper.java
+++ trx/src/main/java/io/sece/vlc/trx/Sleeper.java
@@ -3,28 +3,29 @@ package io.sece.vlc.trx;
 import java.util.concurrent.TimeUnit;
 
 public class Sleeper {
-    private static final long SLEEP_PRECISION = TimeUnit.MILLISECONDS.toNanos(2);
-    private static final long SPIN_YIELD_PRECISION = TimeUnit.MILLISECONDS.toNanos(2);
+    private static final long BUSY_WAITING_THRESHOLD = TimeUnit.MILLISECONDS.toNanos(2);
 
     /* Spin-yield loop based alternative to Thread.sleep
      * Based on the code of Andy Malakov
      * http://andy-malakov.blogspot.fr/2010/06/alternative-to-threadsleep.html
      */
-    public static void sleepNanos(long nanoDuration) throws InterruptedException {
-        final long end = System.nanoTime() + nanoDuration;
-        long timeLeft = nanoDuration;
+    public static void sleepNanos(long duration) throws InterruptedException {
+        final long end = System.nanoTime() + duration;
+        long left = duration;
+
         do {
-            if (timeLeft > SLEEP_PRECISION) {
-                Thread.sleep(1);
-            } else {
-                if (timeLeft > SPIN_YIELD_PRECISION) {
-                    Thread.yield();
-                }
-            }
-            timeLeft = end - System.nanoTime();
+            if (left > BUSY_WAITING_THRESHOLD)
+                Thread.sleep(TimeUnit.NANOSECONDS.toMillis(left - BUSY_WAITING_THRESHOLD));
 
             if (Thread.interrupted())
                 throw new InterruptedException();
-        } while (timeLeft > 0);
+
+            left = end - System.nanoTime();
+        } while (left > 0);
+    }
+
+
+    public static void sleep(long duration, TimeUnit unit) throws InterruptedException {
+        sleepNanos(unit.toNanos(duration));
     }
 }
--- trx/src/main/java/io/sece/vlc/trx/Transmitter.java
+++ trx/src/main/java/io/sece/vlc/trx/Transmitter.java
@@ -1,5 +1,7 @@
 package io.sece.vlc.trx;
 
+import java.util.concurrent.TimeUnit;
+
 import io.sece.vlc.Coordinate;
 import io.sece.vlc.Modem;
 
@@ -11,45 +13,44 @@ import io.sece.vlc.Modem;
 class Transmitter<T extends Coordinate> {
     private LEDInterface<T> led;
     private Modem<T> modem;
-    private int interval;
+    private long interval;
 
 
-
-    public Transmitter(LEDInterface<T> led, Modem<T> modem, int interval)
+    public Transmitter(LEDInterface<T> led, Modem<T> modem, int interval, TimeUnit unit)
     {
         this.led = led;
         this.modem = modem;
-        this.interval = interval;
+        this.interval = unit.toNanos(interval);
     }
+
+
+    public Transmitter(LEDInterface<T> led, Modem<T> modem, int interval) {
+        this(led, modem, interval, TimeUnit.MILLISECONDS);
+    }
+
 
     public void tx(String data) throws LEDException, InterruptedException
     {
-        if(data.length() % modem.bits == 0)
-        {
-            long startTime = System.currentTimeMillis();
-            long delta = 0;
-            int count = 1;
-            
-            for(int i = 0; i < data.length(); i += modem.bits)
-            {
-                led.set(modem.modulate(data, i));
-                Sleeper.sleepNanos((interval - delta) * 1000000);
-                delta = System.currentTimeMillis() - (startTime + (interval * count));
-                count ++;
-            }
-        }
-        else
-        {
-            throw new IllegalArgumentException();
+        long deadline;
+
+        if (data.length() % modem.bits != 0)
+            throw new IllegalArgumentException("Invalid number of bits");
+
+        int n = data.length() / modem.bits;
+        long start = System.nanoTime();
+
+        for (int i = 0; i < n; i++) {
+            led.set(modem.modulate(data, i * modem.bits));
+            deadline = start + (interval * (i + 1));
+            Sleeper.sleepNanos(deadline - System.nanoTime());
         }
     }
+
 
     public void startTx() throws LEDException, InterruptedException
     {
         int amount = 8; //Amount of symbols should be calculated through interval
-
         this.tx(modem.startSequence(amount));
         this.tx("1000001");
-
     }
 }
