57bf22d43a76f8c0fd741599da98d15b8b295fa4
Jan Janak <jan@janakj.org>
2018-07-15 12:02:57 -0400

    Fix the frame sampler.
    The previous implementation did not work correctly. It would update
    the timestamp of the last taken frame to the timestamp from the image,
    which is not correct. This implementation works with a fixed timestamp
    base and monotonically increases the base with given intervals,
    regardless of the timestamp value of the incoming frame.

--- app/src/main/java/io/sece/vlc/rcvr/processing/block/FrameSampler.java
+++ app/src/main/java/io/sece/vlc/rcvr/processing/block/FrameSampler.java
@@ -1,67 +1,32 @@
 package io.sece.vlc.rcvr.processing.block;
 
-import com.google.common.eventbus.Subscribe;
-
-import java.util.concurrent.TimeUnit;
-
-import io.sece.vlc.Color;
-import io.sece.vlc.Modem;
-import io.sece.vlc.rcvr.Bus;
-import io.sece.vlc.rcvr.ViewfinderModel;
 import io.sece.vlc.rcvr.processing.Frame;
 import io.sece.vlc.rcvr.processing.ProcessingBlock;
 
 
 
 public class FrameSampler implements ProcessingBlock {
-
     private long interval;  // Sampling interval in nano seconds
-    private long lastAcceptedFrame = 0;
-    private Modem<Color> modem;
-    private Color prevColor;
-    private int sameColorCounter = 0;
+    private long next = 0;
 
-    public FrameSampler(int baudRate, Modem modem) {
-        this.interval = 1000000000 / baudRate;
-        this.modem = modem;
-        Bus.subscribe(this);
+
+    public FrameSampler(int baudRate) {
+        setBaudRate(baudRate);
     }
 
 
-    @Subscribe
-    public void onBaudRateChange(Bus.BaudRateChange event) {
-        this.interval = 1000000000 / event.baudRate;
+    public void setBaudRate(long baudRate) {
+        this.interval = 1000000000 / baudRate;
+        next = System.nanoTime();
     }
 
 
     public Frame apply(Frame frame) {
-        Color currColor = frame.getColorAttr(Frame.HUE);
-        currColor = modem.detect(currColor);
+        long ts = frame.getLongAttr(Frame.IMAGE_TIMESTAMP);
 
-        if(prevColor != null){
-            long stamp = frame.getLongAttr(Frame.IMAGE_TIMESTAMP);
-            if(prevColor == currColor){
-                sameColorCounter++;
-            }else{
-//                System.out.println("color processed: " + sameColorCounter + " " + modem.demodulate(currColor));
-                sameColorCounter = 0;
-            }
+        if (ts < next) return null;
 
-
-            if (stamp < lastAcceptedFrame + interval){
-                prevColor = currColor;
-                return null;
-            }
-            lastAcceptedFrame = stamp;
-            prevColor = currColor;
-//            System.out.println("color processed: " + sameColorCounter + " " + modem.demodulate(currColor));
-            sameColorCounter = 0;
-            return frame;
-
-        } else {
-            prevColor = currColor;
-            return null;
-        }
-
+        next += interval;
+        return frame;
     }
 }
