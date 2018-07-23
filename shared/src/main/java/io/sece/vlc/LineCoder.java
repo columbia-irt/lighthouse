b07ec5aa3da39951d8e62c5f694687d775e79a5e
Jan Janak <jan@janakj.org>
2018-07-22 21:13:37 -0400

    Update LineCoder to the latest API.
    In addition, this patch also includes small optimization to the
    internal state machines.

--- shared/src/main/java/io/sece/vlc/LineCoder.java
+++ shared/src/main/java/io/sece/vlc/LineCoder.java
@@ -1,123 +1,121 @@
 package io.sece.vlc;
 
+import java.util.ArrayList;
+import java.util.List;
 
-import io.sece.vlc.modem.FSK4Modem;
-import io.sece.vlc.modem.FSK8Modem;
 
 public class LineCoder {
-    private String frame_marker = "";
+    private static final int NO_MAX_SIZE = -1;
+    private static final int STATE_START = 0;
+    private static final int STATE_S1 = 1;
+    private static final int STATE_S2 = 2;
+    private static final int STATE_D = 3;
+    private static final int STATE_DS1 = 4;
+    private static final int STATE_DS2 = 5;
+    private static final int STATE_RESET = 6;
+    private static final int STATE_D1 = 1;
 
-    private static final int START = 0;
-    private static final int RX_STATE_S1 = 1;
-    private static final int RX_STATE_S2 = 2;
-    private static final int RX_STATE_D = 3;
-    private static final int RX_STATE_DS1 = 4;
-    private static final int RX_STATE_DS2 = 5;
-    private static final int TX_STATE_D1 = 1;
+    private static final class FrameTooLong extends RuntimeException { }
 
-    private static final class FrameTooLong extends IndexOutOfBoundsException { }
+    public final int[] marker;
+    private int state = STATE_START;
+    private final int maxSize;
 
-    private int maxFrameSize;
-    private int state = START;
-    private StringBuilder buffer = new StringBuilder();
-    private Modem<Color> modem;
+    private final ArrayList<Integer> buffer = new ArrayList<>();
 
-    /*
-        Colors for Startingsequence
-     */
-    private Color color1;
-    private Color color2;
-    private Color color3;
 
-    public LineCoder(Modem modem, int maxFrameSize) {
-        this.modem = modem;
-        this.maxFrameSize = maxFrameSize * 8;
-        if(modem instanceof FSK4Modem)
-            initFrameMarker(((FSK4Modem) modem).e, ((FSK4Modem) modem).w, ((FSK4Modem) modem).s);
-        if(modem instanceof FSK8Modem)
-            initFrameMarker(((FSK8Modem) modem).color[FSK8Modem.E], ((FSK8Modem) modem).color[FSK8Modem.W], ((FSK8Modem) modem).color[FSK8Modem.SE]);
+    public LineCoder(int[] marker) {
+        this(marker, NO_MAX_SIZE);
     }
 
-    public void initFrameMarker(Color color1, Color color2, Color color3){
-        this.color1 = color1;
-        this.color2 = color2;
-        this.color3 = color3;
 
-        frame_marker = modem.demodulate(color1) + modem.demodulate(color2) + modem.demodulate(color3);
-        System.out.println("FrameMarker: " + frame_marker);
-    }
+    public LineCoder(int[] marker, int maxSize) {
+        if (marker.length != 3)
+            throw new IllegalArgumentException("Bug: Frame marker must consist of three symbols");
 
-    public String getCurrentData() {
-        return buffer.toString();
+        this.marker = marker.clone();
+        this.maxSize = maxSize;
     }
 
 
     public void reset() {
-        reset(RX_STATE_D);
+        reset(STATE_START);
     }
 
 
     private void reset(int state) {
-        buffer.setLength(0);
+        buffer.clear();
         this.state = state;
     }
 
 
-    public BitString rx(Color color) {
+    public List<Integer> get() {
+        return buffer;
+    }
+
+
+    private void store(int... symbols) {
+        if (maxSize != NO_MAX_SIZE && (buffer.size() + symbols.length) > maxSize)
+            throw new FrameTooLong();
+
+        for (int s : symbols) buffer.add(s);
+    }
+
+
+    public List<Integer> decode(int symbol) {
         try {
             switch (state) {
-                case START:
-                    if (color.equals(color1)) state = RX_STATE_S1;
+                case STATE_START:
+                    if (symbol == marker[0]) state = STATE_S1;
                     break;
 
-                case RX_STATE_S1:
-                    if(color.equals(color2)){
-                        state = RX_STATE_S2;
-                    }else if(color.equals(color1)){
-                        state = RX_STATE_S1;
-                    }else{
-                        state = START;
-                    }
-                    break;
-
-                case RX_STATE_S2:
-                    if(color.equals(color3)){
-                        state = RX_STATE_D;
-                    }else if(color.equals(color1)){
-                        state = RX_STATE_S1;
-                    }else{
-                        state = START;
-                    }
-                    break;
-
-                case RX_STATE_D:
-                    if (color.equals(color1))
-                        state = RX_STATE_DS1;
-                    else
-                        store(modem.demodulate(color));
-                    break;
-
-                case RX_STATE_DS1:
-                    if (color.equals(color2)) {
-                        state = RX_STATE_DS2;
-                    } else if (color.equals(color1)) {
-                        store(modem.demodulate(color));
+                case STATE_S1:
+                    if (symbol == marker[1]) {
+                        state = STATE_S2;
                     } else {
-                        store(modem.demodulate(color1) + modem.demodulate(color));
-                        state = RX_STATE_D;
+                        state = STATE_START;
                     }
                     break;
 
-                case RX_STATE_DS2:
-                    if (color.equals(color3)) {
-                        BitString rv = new BitString(buffer.toString());
-                        reset();
-                        return rv;
-                    } else if (color.equals(color2)) {
-                        store(modem.demodulate(color1) + modem.demodulate(color2));
-                        state = RX_STATE_D;
+                case STATE_S2:
+                    if (symbol == marker[2]) {
+                        state = STATE_D;
+                    } else if (symbol == marker[0]) {
+                        state = STATE_S1;
                     } else {
-                        reset(START);
+                        state = STATE_START;
+                    }
+                    break;
+
+                case STATE_RESET:
+                    reset(STATE_D);
+                    /* fall through to STATE_D */
+
+                case STATE_D:
+                    if (symbol == marker[0]) state = STATE_DS1;
+                    else store(symbol);
+                    break;
+
+                case STATE_DS1:
+                    if (symbol == marker[1]) {
+                        state = STATE_DS2;
+                    } else if (symbol == marker[0]) {
+                        store(symbol);
+                    } else {
+                        store(marker[0], symbol);
+                        state = STATE_D;
+                    }
+                    break;
+
+                case STATE_DS2:
+                    if (symbol == marker[2]) {
+                        state = STATE_RESET;
+                        return buffer;
+                    } else if (symbol == marker[1]) {
+                        store(marker[0], marker[1]);
+                        state = STATE_D;
+                    } else {
+                        reset(STATE_START);
                     }
                     break;
 
@@ -125,58 +123,43 @@ public class LineCoder {
                     throw new RuntimeException("Bug: Invalid state reached");
             }
         } catch (FrameTooLong e) {
-            reset(START);
+            reset(STATE_START);
         }
 
         return null;
     }
 
 
-    private void store(String input) {
-        if (input.length() + buffer.length() > maxFrameSize)
+    public List<Integer> encode(List<Integer> symbols) {
+        if (maxSize != NO_MAX_SIZE && maxSize < symbols.size())
             throw new FrameTooLong();
 
-        buffer.append(input);
-    }
+        reset();
 
+        for (int s : marker) buffer.add(s);
 
-    public String tx(BitString data) {
-        state = START;
+        for (int s : symbols) {
+            buffer.add(s);
+            switch (state) {
+                case STATE_START:
+                    if (s == marker[0]) state = STATE_D1;
+                    break;
 
-        buffer.setLength(0);
-        buffer.append(frame_marker);
+                case STATE_D1:
+                    if (s == marker[1]) {
+                        buffer.add(marker[1]);
+                        state = STATE_START;
+                    } else if (s == marker[0]) {
+                        state = STATE_D1;
+                    } else {
+                        state = STATE_START;
+                    }
+                    break;
 
-        String input = data.toString();
-
-        for(int i = 0; i < input.length(); i += modem.bits) {
-            tx(buffer, modem.modulate(input.substring(i, i + modem.bits)));
+                default:
+                    throw new RuntimeException("Bug: Invalid state reached");
+            }
         }
-
-        return buffer.toString();
-    }
-
-
-    private void tx(StringBuilder b, Color s) {
-        switch(state) {
-            case START:
-                if (s.equals(color1)) state = TX_STATE_D1;
-                b.append(modem.demodulate(s));
-                break;
-
-            case TX_STATE_D1:
-                if (s.equals(color2)) {
-                    b.append(modem.demodulate(s) + modem.demodulate(s));
-                    state = START;
-                } else if(s.equals(color1)) {
-                    b.append(modem.demodulate(s));
-                    state = TX_STATE_D1;
-                } else {
-                    b.append(modem.demodulate(s));
-                    state = START;
-                }
-                break;
-            default:
-                throw new RuntimeException("Bug: Invalid state reached");
-        }
+        return buffer;
     }
 }
