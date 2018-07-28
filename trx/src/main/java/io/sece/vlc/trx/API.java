8f076203bb191b0520f784bad00c8e17b79d18c2
ninja2009 <hagen-ninja@hotmail.de>
2018-07-28 21:00:43 +0200

    Anode implemented

--- trx/src/main/java/io/sece/vlc/trx/API.java
+++ trx/src/main/java/io/sece/vlc/trx/API.java
@@ -40,6 +40,8 @@ public class API {
         server.createContext("/off", new offHandler());
         server.createContext("/webled", new WebLED());
         server.setExecutor(null);
+
+        Main.led.set(Color.BLACK);
     }
 
 
